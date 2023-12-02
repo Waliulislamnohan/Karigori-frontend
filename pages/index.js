7728395409bc161671dd783ded5ba8f748c7cd72
MD Waliul Islam Nohan <waliulnohan@gmail.com>
2023-12-03 00:00:22 +0600

    up

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import { useState, useEffect } from 'react';
 
-import Script from 'next/script';
+
 import Design from './components/Design';
 import KarigoriJobDatabase from './KarigoriJobDatabase.js';
 
